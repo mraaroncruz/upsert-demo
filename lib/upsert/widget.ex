2ac867dd82810a91a5f0f065875aefe5713e3236
Aaron Cruz <elegant.year7264@fastmail.com>
2024-11-12 10:59:18 +0100

    chore: rename some things, fix other things

--- lib/upsert/widget.ex
+++ lib/upsert/widget.ex
@@ -3,20 +3,21 @@ defmodule Upsert.Widget do
   alias Upsert.Widgets.Widget
 
   def add(topic, group, offset) do
-    cs = Widget.changeset(%Widget{}, %{topic: topic, group: group, offset: offset})
+    cs =
+      Widget.changeset(%Widget{}, %{topic: topic, group: group, last_handled_event_offset: offset})
 
-    Repo.insert!(
+    Repo.insert(
       cs,
-      on_conflict: {:replace, [:offset]},
+      on_conflict: {:replace, [:last_handled_event_offset]},
       conflict_target: [:topic, :group]
     )
-
-    Repo.all(Widget)
   end
 
   def add_unsafe(topic, group, offset) do
-    cs = Widget.changeset(%Widget{}, %{topic: topic, group: group, offset: offset})
-    Repo.insert!(cs)
-    Repo.all(Widget)
+    cs =
+      Widget.changeset(%Widget{}, %{topic: topic, group: group, last_handled_event_offset: offset})
+
+    Repo.insert(cs)
+    # Repo.all(Widget)
   end
 end
--- lib/upsert/widgets/widget.ex
+++ lib/upsert/widgets/widget.ex
@@ -6,12 +6,13 @@ defmodule Upsert.Widgets.Widget do
   schema "widgets" do
     field :topic, :string, primary_key: true
     field :group, :string, primary_key: true
-    field :offset, :integer
+    field :last_handled_event_offset, :integer
   end
 
   def changeset(widget, attrs) do
     widget
-    |> cast(attrs, [:topic, :group, :offset])
-    |> validate_required([:topic, :group, :offset])
+    |> cast(attrs, [:topic, :group, :last_handled_event_offset])
+    |> validate_required([:topic, :group, :last_handled_event_offset])
+    |> unique_constraint([:topic, :group], name: "widgets_pkey")
   end
 end
